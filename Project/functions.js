92ed5d49da472f29bd3fed7f447bb18d33ccdf47
hugh <hughy18029@gmail.com>
2020-04-20 17:31:47 -0700

    Dropdown now closes when clicked outside

--- Project/functions.js
+++ Project/functions.js
@@ -6,21 +6,21 @@ function dropdown() {
     checkboxes.style.display = "block";
     expanded = true;
   } else {
-    closeDropDown();
+    checkboxes.style.display = "none";
     expanded = false;
   }
 }
 
-function closeDropDown(){
-    checkboxes.style.display = "none";
-}
 
-window.onclick = function(event) {
-   //  var selectBox = document.getElementsByClassName("selectBox");
-    if (event.target != selectBox) {
-        this.closeDropDown();
-        // checkboxes.style.display = "none";
-    
-  }
-}
 
+window.addEventListener('mouseup', function(event) {
+    var box = document.getElementById("checkboxes");
+    var checks = document.getElementById("text");
+    var i;
+      if(event.target != box && event.target.parentNode != box){
+        if (event.target != checks && event.target.parentNode != checks){
+          this.console.log(5);
+          box.style.display = "none";
+        }  
+      }
+  });
